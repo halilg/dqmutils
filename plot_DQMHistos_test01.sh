2514f7a78d7060cbc0eda206bec4a3718938409d
Marino Missiroli <m.missiroli@cern.ch>
2019-03-24 17:27:01 +0100

    added plot_DQMHistos_test01.sh

---
+++ plot_DQMHistos_test01.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+INPUT_FILE=DQM_V0001_R000000001__Global__CMSSW_X_Y_Z__RECO.root
+
+OUTPUT_DIR=tmp
+
+if [ -f ${INPUT_FILE} ]; then
+
+  ./plot_DQMHistos.py -i ${INPUT_FILE} -o ${OUTPUT_DIR} -e png --only-keys '/DQM/' '/TOP/'
+fi
+
+unset -v INPUT_FILE
