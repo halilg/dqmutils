27f122f988894a400305241fd576ceb4b3f4eb07
Halil Gamsizkan <halil.gamsizkan@cern.ch>
2021-03-04 09:48:09 +0100

    new CMSSW version

--- setup_CMSSW.sh
+++ setup_CMSSW.sh
@@ -18,7 +18,7 @@ fi
 cd ${BASE_DIR}
 unset -v BASE_DIR
 
-CMSSW_rel=CMSSW_11_0_0_pre5
+CMSSW_rel=CMSSW_11_3_0_pre3
 
 if [ ! -d ${CMSSW_rel} ]; then
   cmsrel  ${CMSSW_rel}
@@ -41,7 +41,11 @@ if [ ! -d DQMOffline/Trigger ]; then git cms-addpkg DQMOffline/Trigger; fi;
 # Validation step
 if [ ! -d HLTriggerOffline ]; then git cms-addpkg HLTriggerOffline; fi;
 
-scram b
+
+# checkout latest packages
+git pull https://github.com/missirol/cmssw.git hltjme_dqm_1130pre3
+
+scram b -j 10
 scram b
 
 cd ${CMSSW_BASE}/..
